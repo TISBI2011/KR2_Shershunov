60dafd98675af11bd68142e3100fd10e7765f6f0
ИС201-1 <IS201-1@tisbi.ru>
2024-02-01 16:15:58 +0300

    “Program”

---
+++ KR2_ShershunovD/KR2_ShershunovD/Pages/LoginPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KR2_ShershunovD.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();            
+        }
+
+        private void BLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string login = TBLogin.Text;
+            string password = TBPassword.Text;
+
+            var loggedUser = App.DB.User.FirstOrDefault(x => x.Login == login && x.Password == password);
+
+            if(loggedUser == null)
+            {
+                MessageBox.Show("Введены неправильно логин или пароль проверьте правильность данных");
+                return;
+            }
+            App.LoggedUser = loggedUser;
+            NavigationService.Navigate(new MainPage());
+        }
+
+        private void BRegistration_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RegistrationPages());
+        }
+    }
+}
---
+++ KR2_ShershunovD/KR2_ShershunovD/Pages/RegistrationPages.xaml.cs
@@ -0,0 +1,44 @@
+﻿using KR2_ShershunovD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KR2_ShershunovD.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationPages.xaml
+    /// </summary>
+    public partial class RegistrationPages : Page
+    {
+        User contextUser;
+        public RegistrationPages()
+        {
+            InitializeComponent();
+            CBRole.ItemsSource = App.DB.Role.ToList();
+            contextUser = new User();
+            DataContext = contextUser;
+        }
+
+        private void BSave_Click(object sender, RoutedEventArgs e)
+        {
+            App.DB.User.Add(contextUser);
+            App.DB.SaveChanges();            
+        }
+
+        private void BBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
